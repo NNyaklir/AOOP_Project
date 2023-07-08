c0052c26114e8c001872311f90d433f727f0b684
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-07 19:47:20 -0600

    added variables and methods
    added name and address variables, and relevant get and set methods

--- Person.java
+++ Person.java
@@ -1,4 +1,25 @@
 
 public class Person {
+    protected String name;
+    protected String address;
     
+    /**@param s name */
+    protected void setName(String s){
+        this.name=s;
+    }
+    
+    /**@return name */
+    protected String getName(){
+        return name;
+    }
+
+    /**@param a person's address */
+    protected void setAddress(String a){
+        this.address=a;
+    }
+
+    /**@return person's address */
+    protected String getAddress(){
+        return address;
+    }
 }
