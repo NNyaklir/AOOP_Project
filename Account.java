8ebca0cbb7c45ae0deb410463d6968028acf0cd7
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-07 19:19:14 -0600

    added relevant methods
    Created get and set methods for account number, customer, and balance

--- Account.java
+++ Account.java
@@ -5,4 +5,33 @@ public abstract class Account{
     protected Customer customer;
     protected double balance;
 
+    /** @param n new account number */
+    protected void setAccountNumber(int n){
+        this.accountNumber=n;
+    }
+
+    /**@return the account number */
+    protected int getAccountNumber(){
+        return accountNumber;
+    }
+
+    /** @param customer */
+    protected void setCustomer(Customer c){
+        this.customer=c;
+    }
+
+    /**@return customer */
+    protected Customer getCustomer(){
+        return customer;
+    }
+    
+    /** @param bal sets initail balance */
+    protected void setBalance(double bal){
+        this.balance=bal;
+    }
+
+    /** @return current balance */ 
+    protected double getBalance(){
+        return balance;
+    }
 }
