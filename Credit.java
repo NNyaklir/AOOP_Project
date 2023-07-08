a39eae286d379d59ff096f66c155b66b0a50c0b7
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-07 19:40:00 -0600

    Added methods
    Added get and set for max credit, added extend credit method to increase max credit

--- Credit.java
+++ Credit.java
@@ -1,4 +1,31 @@
 
 public class Credit extends Account{
+    protected double maxCredit;//should be a negative double amount that cannot be surpassed
+    
+
+    /**@return current max credit */
+    protected double getMaxCredit(){
+        return maxCredit;
+    }
+
+    /**@param maxC max credit amount for current  */
+    protected void setMaxCredit(double maxC){
+        this.maxCredit=maxC;
+    }
+
+    /**@param addlCredit increased credit value 
+     * increases the max credit limit on account
+    */
+    protected void extendCredit(double maxC){
+        try{
+            if(balance>=maxC && maxCredit>maxC){
+                maxCredit=maxC;
+            }
+        }
+        catch( Exception e){
+            System.out.println("Max Credit cannot be extended below current limit");
+        }
+    }
+
     
 }
