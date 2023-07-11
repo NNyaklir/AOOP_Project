e4562427428711139cee025ddfa70c39c6058127
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-11 12:52:47 -0600

    Update Customer.java
    Added values for accounts and relevant methods

--- Customer.java
+++ Customer.java
@@ -1,4 +1,36 @@
 
 public class Customer extends Person {
+    protected Checking custChecking;
+    protected Credit custCredit;
+    protected Saving custSaving;
     
+    /**@param c checking acc */
+    protected void setChecking(Checking c){
+        this.custChecking=c;
+    }
+
+    /**@return checking account */
+    protected Checking getChecking(){
+        return custChecking;
+    }
+
+    /**@param c credit acc */
+    protected void setCredit(Credit c){
+        this.custCredit=c;
+    }
+
+    /**@return credit account */
+    protected Credit getCredit(){
+        return custCredit;
+    }
+
+    /**@param s saving account */
+    protected void setSaving(Saving s){
+        this.custSaving=s;
+    }
+
+    /**@return saving account */
+    protected Saving getSaving(){
+        return custSaving;
+    }
 }
