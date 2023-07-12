f023702da3786a9c5a2baf63e4740d3e8e6486cc
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-12 10:17:12 -0600

    Update CsvImporter.java
    In theory this should work, needs testing

--- CsvImporter.java
+++ CsvImporter.java
@@ -7,7 +7,6 @@ public class CsvImporter {
     protected ArrayList<Saving> savingList;
     protected ArrayList<Credit> creditList;
 
-
     /** method to import csv files, this will be hardcoded. should only be used once. */
     protected void importCSV(){
 
@@ -20,100 +19,72 @@ public class CsvImporter {
             reader=new BufferedReader(new FileReader(file));
 
             while((line=reader.readLine()) !=null){
+                String[] values=line.split(",");
 
                 int i=0;//there are 13 things to go through
-                int j=0;
-                if(j==0){
-                    while(i<=12){
-                        String[] row=line.split(",");
-                        i++;
-                    }
-                    j+=1;  
-                    i=0;
-                }
-                else{
+                int j=12;
+                
+                for(int k=0;k<values.length;k++){
                     switch(i){
                         //yes i know there are a lot of errors ill fix it before final push, this is more like skeleton than anything else rn
                         case 0:
                         Customer cust= new Customer();
-                        cust.id=Integer.parseInt(line.split(","));
-                        i++;
-                        break;
-
-                        case 1:
-                        cust.nameFirst=line.split(",");
-                        i++;
-                        break;
-
-                        case 2:
-                        cust.nameLast=line.split(",");
-                        i++;
-                        break;
-
-                        case 3:
-                        cust.dob=line.split(",");
-                        i++;
-                        break;
-
-                        case 4:
-                        cust.address=line.split(",");
-                        i++;
-                        break;
-
-                        case 5:
-                        cust.phoneNumber=line.split(",");
+                        cust.id=Integer.parseInt(values[i+j]);
+                        i++; k++;
+                        
+                        cust.nameFirst=values[i+j];
+                        i++;k++;
+                        
+                        cust.nameLast=values[i+j];
+                        i++;k++;
+                        
+                        cust.dob=values[i+j];
+                        i++; k++;
+                        
+                        cust.address=values[i+j];
+                        i++; k++;
+                        
+                        cust.phoneNumber=values[i+j];
                         custList.add(cust);
-                        i++;
-                        break;
-
-                        case 6:
+                        i++; k++;
+                        
+                        //starts checking accounts
                         Checking check= new Checking();
                         check.customer=cust;
                         cust.setChecking(check);
-                        check.accountNumber=line.split(",");
-                        i++;
-                        break;
-
-                        case 7:
-                        check.balance=line.split(",");
+                        check.accountNumber=Integer.parseInt(values[i+j]);
+                        i++;k++;
+                        
+                        check.balance=Integer.parseInt(values[i+j]);
                         checkingList.add(check);
-                        i++;
-                        break;
-
-                        case 8:
+                        i++;k++;
+                        
+                        //starts saving accounts
                         Saving sav= new Saving();
                         sav.customer=cust;
                         cust.setSaving(sav);
-                        sav.accountNumber=line.split(",");
-                        i++;
-                        break;
-
-                        case 9:
-                        sav.balance=line.split(",");
+                        sav.accountNumber=Integer.parseInt(values[i+j]);
+                        i++;k++;
+                        
+                        sav.balance=Integer.parseInt(values[i+j]);
                         savingList.add(sav);
-                        i++;
-                        break;
+                        i++;k++;
+                        
 
-                        case 10:
+                        //starts credit accounts
                         Credit cred = new Credit();
                         cred.customer=cust;
                         cust.custCredit=cred;
-                        cred.accountNumber=line.split(",");
-                        i++;
-                        break;
+                        cred.accountNumber=Integer.parseInt(values[i+j]);
+                        i++;k++;
+                        
+                        cred.maxCredit=Integer.parseInt(values[i+j]);
+                        i++;k++;
+                        
 
-                        case 11:
-                        cred.maxCredit=line.split(",");
-                        i++;
-                        break;
-
-                        case 12:
-                        cred.balance=line.split(",");
-                        i=0;
+                        cred.balance=Integer.parseInt(values[i+j]);
+                        i=0;k++;
                         creditList.add(cred);
-                        break;
-
-
                     }
                 } 
             }
@@ -121,10 +92,13 @@ public class CsvImporter {
         catch(Exception e){
             e.printStackTrace();
         }
-
         finally{
+            try{
             reader.close();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
         }
-    }
-    
+    }   
 }
