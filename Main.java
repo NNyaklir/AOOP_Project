6153084d512c63d6be5a8f480e7a5d1954fa5bce
amart251 <amartinez251@miners.utep.edu>
2023-07-13 14:22:08 -0600

    Updated main so that it is less cluttered. Also included a "who are you" option

--- Main.java
+++ Main.java
@@ -1,39 +1,51 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         boolean run = true;
         Scanner userInput = new Scanner(System.in);
-        while (run){
-            System.out.println("A. Inquire account by name. \n" +
-                    "B. Inquire account by type/number.");
 
-            String enterProgram = userInput.nextLine(); // Initialize Scanner
+        System.out.println("Welcome to the El Paso Miners Banking system");
+        System.out.println("please log-in below");
+        System.out.println("1. Customer log-in \n" +
+                "2. Bank Management log-in");
 
-            if (enterProgram.equalsIgnoreCase("A")){ // Look Up Name
-                System.out.print("Enter Account Name: ");
-                enterProgram = userInput.nextLine();
-                // Look Up If Name has a corresponding account
+        while (run) {
+            String loginType = userInput.nextLine();
 
-            } else if (enterProgram.equalsIgnoreCase("B")){ // Look Up Account
-                System.out.println("What is the account type?");
-                enterProgram = userInput.nextLine();
-
-                System.out.println("What is the account number?");
-                enterProgram = userInput.nextLine();
-
-                // Display Corresponding Account
-
-            } else if(enterProgram.equalsIgnoreCase("Exit")){ // End Program
+            if (loginType.equalsIgnoreCase("1")) { // directly into inqury if customer
+                inquireAccount(userInput);
+            } else if (loginType.equalsIgnoreCase("2")) { // echos message for management and then into inquiry
+                System.out.println("Hello Management, what would you like to enquire about your client?");
+                inquireAccount(userInput);
+            } else if (loginType.equalsIgnoreCase("Exit")) {
                 System.out.println("Goodbye!");
                 run = false;
-            } else{
-                System.out.println("Invalid Input");
+            } else {
+                System.out.println("Invalid input, please try again.");
             }
+        }
 
+        userInput.close();
+    }
 
+    public static void inquireAccount(Scanner userInput) { // moved the account inquiry to its own separate method to
+                                                           // declutter
+        System.out.println("A. Inquire account by name. \n" +
+                "B. Inquire account by type/number.");
+        String inquiryType = userInput.nextLine();
 
+        if (inquiryType.equalsIgnoreCase("A")) {
+            System.out.print("Enter Account Name: ");
+            String accountName = userInput.nextLine();// Look Up If Name has a corresponding account
 
-        userInput.close();//closed this bc it bugs me -Devin
-        } // end of program
+        } else if (inquiryType.equalsIgnoreCase("B")) {
+            System.out.println("What is the account type?");
+            String accountType = userInput.nextLine();
+
+            System.out.println("What is the account number?");
+            String accountNumber = userInput.nextLine();// Display Corresponding Account
+            System.out.println("Welcome, " + "INSERT USERNAME");
+        }
     }
 }
