d0d666578c5e08110e5d8079c942eb60f8dd4068
amart251 <amartinez251@miners.utep.edu>
2023-07-14 01:21:54 -0600

    Extended functionality in UI main. Introduced money services method, however still needs work. Updated TODO.

--- Main.java
+++ Main.java
@@ -29,15 +29,24 @@ public class Main {
         userInput.close();
     }
 
+    // bug found: if 1 oe 2 is pressed at any moment, it takes it as if you were
+    // re-loging in as a cust. or BM and gets you back to these options
     public static void inquireAccount(Scanner userInput) { // moved the account inquiry to its own separate method to
                                                            // declutter
+                                                           // @TODO: Need to pull bank user info into this method.
         System.out.println("A. Inquire account by name. \n" +
                 "B. Inquire account by type/number.");
         String inquiryType = userInput.nextLine();
 
-        if (inquiryType.equalsIgnoreCase("A")) {
+        if (inquiryType.equalsIgnoreCase("A")) { // chaned this so that in order to log in, we require with name and acc
+                                                 // num or type and acc num
             System.out.print("Enter Account Name: ");
             String accountName = userInput.nextLine();// Look Up If Name has a corresponding account
+
+            System.out.println("What is the account number?");
+            String accountNumber = userInput.nextLine();// Display Corresponding Account
+            System.out.println("Welcome, " + "INSERT USERNAME");
+            // @TODO: Need to integrate account information here.
 
         } else if (inquiryType.equalsIgnoreCase("B")) {
             System.out.println("What is the account type?");
@@ -46,6 +55,68 @@ public class Main {
             System.out.println("What is the account number?");
             String accountNumber = userInput.nextLine();// Display Corresponding Account
             System.out.println("Welcome, " + "INSERT USERNAME");
+            // @TODO: Need to integrate account information here.
+
+            System.out.println("Would you like to access our money services? \n" + "Yes \n" + "No \n");
+            Scanner scanner = new Scanner(System.in); // Create a new Scanner object
+
+            if (inquiryType.equalsIgnoreCase("Yes")) {
+                moneyServices(scanner); // sends the client into money services method
+
+            } else if (inquiryType.equalsIgnoreCase("No")) {
+                System.out.println(":)");
+                // @TODO: redisplays account information
+            }
+
         }
     }
+
+    // This method provides the money services to the user.
+    protected static void moneyServices(Scanner userInput) {
+
+        // Ask the user for the account type.
+        System.out.println("What is the account type?");
+        String accountType = userInput.nextLine();
+
+        // Display the options for the user to select the service they want to access.
+        System.out.println("Choose the service you would like to access: \n" +
+                "1. Deposit \n" +
+                "2. Withdraw \n" +
+                "3. Transfer Money");
+
+        // Get the service type from the user input.
+        String serviceType = userInput.nextLine();
+
+        // Use a switch statement to perform actions based on the selected service.
+        switch (serviceType.toLowerCase()) {
+
+            // If the user selects 1, they have chosen to deposit money.
+            case "1":
+                System.out.println("You have chosen to deposit money.");
+                // @TODO: Implement functionality to allow user to deposit money.
+                break;
+
+            // If the user selects 2, they have chosen to withdraw money.
+            case "2":
+                System.out.println("You have chosen to withdraw money.");
+                // @TODO: Implement functionality to allow user to withdraw money.
+                break;
+
+            // If the user selects 3, they have chosen to transfer money.
+            case "3":
+                System.out.println("You have chosen to transfer money.");
+                // @TODO: Implement functionality to allow user to transfer money between
+                // accounts
+                // @TODO: need to also implement money transfer/bill payment between other
+                // people's accounts
+                break;
+
+            // If the user enters any other input, it's considered as invalid and they're
+            // asked to try again.
+            default:
+                System.out.println("Invalid input, please try again.");
+                break;
+        }
+    }
+
 }
--- TODO.md
+++ TODO.md
@@ -14,25 +14,12 @@
         [X] Ask who user is - Adrian
         [X] Ask if bank manager - Adrian
             [ ] Bank managers need admin access to see inquire about any account
-<<<<<<< HEAD
+        [X] Money services UI page in main (needs more work) - Adrian
     [ ] Transaction Logs -Devin
         [ ] Log all transactions
         [ ] Ability to print transaction log
     [X] Exit Program - Adrian
         (needs to update bank usersheet)
-=======
-    [x] Transaction Logs -Devin
-        [x] Log all transactions
-        [ ] Log inquiries
-    [X] Exit Program - Adrian
-        (needs to update bank usersheet)
-    [ ] Other
-        [ ] Integrate operations with UI
-            [ ] Balance inquiry
-            [ ] Deposit
-            [ ] Withdraw
-            [ ] Transfer
->>>>>>> 9b57737091eff2c1f26b0034707559d08f06496b
 
 ## Files
     [ ] Generate Javadoc/ Ensure all files have javadoc comments- Ray
