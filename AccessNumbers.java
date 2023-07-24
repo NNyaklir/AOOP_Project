747f4ead6929e5efdf71e8c09e9259ce7fd6d4de
Devin <60894239+NNyaklir@users.noreply.github.com>
2023-07-23 23:03:14 -0600

    Finished class and testing for AccessNumbers, all functional

--- AccessNumbers.java
+++ AccessNumbers.java
@@ -1,5 +1,7 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /** This is a class that is used to access id/account numbers used for creation of new accounts */
@@ -14,6 +16,7 @@ public class AccessNumbers {
     private static int savNum;
     private static int credNum;
 
+    /**@return the relevant ID number for new customer */
     public int getIDNum(){
         try (BufferedReader reader = new BufferedReader(new FileReader(IDFilepath))){
             String line = reader.readLine();
@@ -24,7 +27,68 @@ public class AccessNumbers {
         catch (IOException | NumberFormatException e){
             System.out.println("Error loading relevant number:"+ e.getMessage());
         }
+        incrementNumAndSave(IDNum, IDFilepath);
         return IDNum;
+    }
+
+    /**@return the relevant Checking number for new customer */
+    public int getCheckNum(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(checkNumFilepath))){
+            String line = reader.readLine();
+            if(line != null){
+                checkNum = Integer.parseInt(line);
+            }
+        }
+        catch (IOException | NumberFormatException e){
+            System.out.println("Error loading relevant number:"+ e.getMessage());
+        }
+        incrementNumAndSave(checkNum, checkNumFilepath);
+        return checkNum;
+    }
+
+    /**@return the relevant Savings number for new customer */
+    public int getSavNum(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(savNumFilepath))){
+            String line = reader.readLine();
+            if(line != null){
+                savNum = Integer.parseInt(line);
+            }
+        }
+        catch (IOException | NumberFormatException e){
+            System.out.println("Error loading relevant number:"+ e.getMessage());
+        }
+        incrementNumAndSave(savNum, savNumFilepath);
+        return savNum;
+    }
+
+    /**@return the relevant Credit number for new customer */
+    public int getCredNum(){
+        try (BufferedReader reader = new BufferedReader(new FileReader(credNumFilepath))){
+            String line = reader.readLine();
+            if(line != null){
+                credNum = Integer.parseInt(line);
+            }
+        }
+        catch (IOException | NumberFormatException e){
+            System.out.println("Error loading relevant number:"+ e.getMessage());
+        }
+        incrementNumAndSave(credNum, credNumFilepath);
+        return credNum;
+    }
+
+    /** @param num the number which needs to be updated
+     * @param filepath relevant filepath of the number that needs to be updated
+     * This method will increment the relevant id/account number and then save it into
+     * the relevant file. Only used in AccessNumbers.java
+     */
+    private void incrementNumAndSave(int num, String filepath){
+        num+=1;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+            writer.write(String.valueOf(num));
+        } catch (IOException e) {
+            // Handle file write errors
+            System.err.println("Error saving last account number: " + e.getMessage());
+        }
     }
 
 }
---
+++ AccessNumbersTest.java
@@ -0,0 +1,18 @@
+
+/** A class to test AccessNumbers.java functionality */
+public class AccessNumbersTest {
+    
+    public static void main(String[]args){
+    AccessNumbers a = new AccessNumbers();
+    int test1 = a.getCheckNum();
+    int test2 = a.getSavNum();
+    int test3 = a.getCredNum();
+    int test4 = a.getIDNum();
+
+    System.out.println(test1);
+    System.out.println(test2);
+    System.out.println(test3);
+    System.out.println(test4);
+    }
+    
+}
