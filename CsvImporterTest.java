f7d3a90ebfde57eaa2c980c05a3abde3ec4ef8a8
rcast915 <138902701+rcast915@users.noreply.github.com>
2023-08-01 13:29:09 -0600

    Added JUnit for CSVImporter and TransactionRunner

---
+++ CsvImporterTest.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+/**Junit Test For CSV importer*/
+public class CsvImporterTest {
+    private CsvImporter importer;
+
+    /**Set Up Method used for test*/
+    @BeforeEach
+    public void setUp() {
+        importer = new CsvImporter();
+        importer.dataImport();
+    }
+
+    /**Test to retrieve first name in CSV */
+    @Test
+    public void testGetNameFirst() {
+        ArrayList<Customer> test = importer.getCustList();
+        assertEquals("Derek", test.get(1).getNameFirst());
+    }
+
+
+    /**Test to set Name in CSV*/
+    @Test
+    public void testSetNameFirst() {
+        ArrayList<Customer> test = importer.getCustList();
+        test.get(1).setNameFirst("Jack");
+        assertEquals("Jack", test.get(1).getNameFirst());
+    }
+
+    /**Test to look up customer by name*/
+    @Test
+    public void testSearchByName() {
+        ArrayList<Customer> another = importer.getCustList();
+        Searcher search = new Searcher();
+        int index = search.searchByName("Cesar", "Cabrera", another);
+        assertEquals(13, index);
+    }
+
+
+    /**Test to look up customer by checking account*/
+    @Test
+    public void testSearchByChecking() {
+        ArrayList<Checking> checkList = importer.getCheckList();
+        Searcher search = new Searcher();
+        int index2 = search.searchByChecking(1075, checkList);
+        assertEquals(3, index2);
+    }
+
+    /**Test to look up by customer by Savings*/
+    @Test
+    public void testSearchBySavings() {
+        ArrayList<Checking> checkList = importer.getCheckList();
+        Searcher search = new Searcher();
+        int index2 = search.searchByChecking(1075, checkList);
+        assertEquals(3, index2);
+    }
+
+
+
+
+}
--- TransactionRunnerTest.java
+++ TransactionRunnerTest.java
@@ -1,13 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**A class to test mechanics of TransactionRunner.java */
+/**JUnit Test for Transaction Runner*/
 public class TransactionRunnerTest {
-    public static void main(String[]args){
+    @Test
+    public void testImportCSVto2darray() {
         TransactionRunner runner = new TransactionRunner();
-        String[][] data=runner.importCSVto2darray();
-        //no errors from this
-
-        System.out.println("test");
-        System.out.println(data[7][1]);
-        //testing determined that for deposits the first 3 columns will be blank and not pose a problem
+        String[][] data = runner.importCSVto2darray();
+        // Test if data is not null
+        assertNotNull(data);
+        // Test if the data array has at least one row and one column
+        assertTrue(data.length > 0);
+        assertTrue(data[0].length > 0);
+        assertNotNull(data[7][1]);
     }
 }
+
